29bcefda69c8cd767757c964bff727cb66f68c2b
bauer-hillary-c <hbauer@ezcater.com>
2017-06-10 22:29:22 -0400

    creates score page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
   root "pages#home"
+  get 'pages/home'
+  get "score" => "pages#score"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
