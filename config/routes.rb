bcbe2a821b50cc32d5f726df542dc4934b6b0d6e
bauer-hillary-c <hbauer@ezcater.com>
2018-01-10 20:35:45 -0500

    creates games#index page and route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   get "score" => "pages#score"
 
+  resources "games", only: [:index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
