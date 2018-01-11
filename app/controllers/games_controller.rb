d41af1d3f0029d8179457516540be58d60021cb2
bauer-hillary-c <hbauer@ezcater.com>
2018-01-10 22:00:01 -0500

    adds new and save controller actions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,17 @@
 class GamesController < ApplicationController
+  def new
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      redirect_to @game
+    else
+      render 'new'
+    end
+  end
+
   def index
     @games = Game.all
   end
