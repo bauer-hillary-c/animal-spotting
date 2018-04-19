88735e58bea67971eb10d28f4c337864e23045a3
bauer-hillary-c <hbauer@ezcater.com>
2018-04-19 17:57:19 -0400

    replaces edit buttons with delete buttons in index

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -22,4 +22,10 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to games_url, notice: 'Game was successfully destroyed.'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "score" => "pages#score"
   post "update_scores" => "pages#update_scores"
 
-  resources :games, only: [:index, :new, :create, :show]
+  resources :games, only: [:index, :new, :create, :show, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
