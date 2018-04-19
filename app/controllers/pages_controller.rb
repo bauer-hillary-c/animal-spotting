7107ae109c39889aa529999d56f18a8caa41f767
bauer-hillary-c <hbauer@ezcater.com>
2018-04-18 23:30:47 -0400

    wires up controller action to update scores

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,8 +8,19 @@ class PagesController < ApplicationController
 
   def update_scores
     @game = Game.find(params[:game_id])
+    @player = @game.users
 
-    # update scores
+    if params[:scores][:player_1_points].present?
+      p1_score = @players.first.scores.find_by(game_id: @game.id)
+      new_player_1_score = (p1_score.points + params[:scores][:player_1_points].to_i)
+      p1_score.update(points: new_player_1_score)
+    end
+
+    if params[:scores][:player_2_points].present?
+      p2_score = @players.last.scores.find_by(game_id: @game.id)
+      new_player_2_score = (p2_score.points + params[:scores][:player_2_points].to_i)
+      p2_score.update(points: new_player_2_score)
+    end
 
     redirect_to game_path(@game.id)
   end
