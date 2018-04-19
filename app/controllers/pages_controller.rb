e085d9422cf60144860ca66e7f7956fe8936a107
bauer-hillary-c <hbauer@ezcater.com>
2018-04-18 23:17:58 -0400

    adds input fields on game#show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,13 +9,7 @@ class PagesController < ApplicationController
   def update_scores
     @game = Game.find(params[:game_id])
 
-    p1_score = @game.users.first.scores.find_by(game_id: @game.id)
-    new_player_1_score = (player_1_score =+ params[:scores][:player_1_points].to_i)
-    p1_score.update(points: new_player_1_score)
-
-    p2_score = @game.users.last.scores.find_by(game_id: @game.id)
-    new_player_2_score = (player_2_score =+ params[:scores][:player_2_points].to_i)
-    p2_score.update(points: new_player_2_score)
+    # update scores
 
     redirect_to game_path(@game.id)
   end
