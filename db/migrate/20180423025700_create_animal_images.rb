94abe1e9072d6ab8bed065cc0873955e5e3cc973
bauer-hillary-c <hbauer@ezcater.com>
2018-04-22 23:01:41 -0400

    create animal_images table

---
+++ db/migrate/20180423025700_create_animal_images.rb
@@ -0,0 +1,9 @@
+class CreateAnimalImages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :animal_images do |t|
+      t.attachment :image
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180422060024) do
+ActiveRecord::Schema.define(version: 20180423025700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "animal_images", force: :cascade do |t|
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
 
   create_table "animals", force: :cascade do |t|
     t.datetime "created_at", null: false
