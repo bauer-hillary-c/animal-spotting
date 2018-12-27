cc0cc164620524156fdb1fa0e93520efff65ae10
bauer-hillary-c <hbauer@ezcater.com>
2018-12-26 22:42:16 -0500

    Adds username to User and allows email or username to be used at login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,32 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, 
+         :validatable, authentication_keys: [:login]
 
   has_many :scores
   has_many :point_values, foreign_key: "created_by_id"
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  validate :validate_username
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 end
