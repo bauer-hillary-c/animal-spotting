67a4bac01123fd6884d225c73ee7f43c12e4bc81
bauer-hillary-c <hbauer@ezcater.com>
2019-01-06 23:50:06 -0500

    Refactors score input and update_score function

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -3,16 +3,12 @@ class ScoresController < ApplicationController
     @game = Game.find(params[:game_id])
     @players = @game.users
 
-    if params[:scores][:player_1_points].present?
-      p1_score = @players.first.scores.find_by(game_id: @game.id)
-      new_player_1_score = (p1_score.points + params[:scores][:player_1_points].to_i)
-      p1_score.update(points: new_player_1_score)
-    end
+    params[:scores].each do |id, score|
+      player = User.find(id)
+      player_score = player.scores.find_by(game_id: @game.id)
+      new_score = player_score.points + score.to_i
 
-    if params[:scores][:player_2_points].present?
-      p2_score = @players.last.scores.find_by(game_id: @game.id)
-      new_player_2_score = (p2_score.points + params[:scores][:player_2_points].to_i)
-      p2_score.update(points: new_player_2_score)
+      player_score.update(points: new_score)
     end
 
     redirect_to game_path(@game.id)
